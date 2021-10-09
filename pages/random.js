9e162d6cb96d9add43229293f9aa060f6e04b406
holyaustin <holyaustin@yahoo.com>
2021-10-09 19:21:12 +0100

    yes

--- pages/random.js
+++ pages/random.js
@@ -5,7 +5,7 @@ import { create as ipfsHttpClient } from 'ipfs-http-client'
 import { useRouter } from 'next/router'
 import Web3Modal from 'web3modal'
 import Image from 'next/image'
-import random from '../images/random1s.jpg'
+import random from '../images/random1.jpg'
 import Head from 'next/head'
 
 const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
